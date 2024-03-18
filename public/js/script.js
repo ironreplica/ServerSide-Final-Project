1696c32894a49e5e088a1cdeef8a52b6f59c8694
Trevor Childs <trevorchilds8@gmail.com>
2024-03-18 10:57:20 -0600

    Sending drawing functionality

--- public/js/script.js
+++ public/js/script.js
@@ -2,95 +2,107 @@
 var canvas = document.getElementById("drawing-canvas");
 
 // canvas context, which is 2d
-var ctx = canvas.getContext('2d');
+var ctx = canvas.getContext("2d");
 resize();
 
-var pos = { x: 0, y: 0};
+var pos = { x: 0, y: 0 };
 
 var reader = new FileReader();
 // resize when window is resized
-window.addEventListener('resize', resize);
+window.addEventListener("resize", resize);
 
-document.addEventListener('mousemove', draw);
-document.addEventListener('mousedown', setPosition);
-document.addEventListener('mousenter', setPosition);
+document.addEventListener("mousemove", draw);
+document.addEventListener("mousedown", setPosition);
+document.addEventListener("mousenter", setPosition);
 
 // setting the mouse pos
-function setPosition(e){
-    // Compensate for container
-    var rect = canvas.getBoundingClientRect();
-    var scaleX = canvas.width / rect.width;
-    var scaleY = canvas.height / rect.height;
+function setPosition(e) {
+  // Compensate for container
+  var rect = canvas.getBoundingClientRect();
+  var scaleX = canvas.width / rect.width;
+  var scaleY = canvas.height / rect.height;
 
-    pos.x = e.offsetX * scaleX;
-    pos.y = e.offsetY * scaleY;
+  pos.x = e.offsetX * scaleX;
+  pos.y = e.offsetY * scaleY;
 }
 
 // size the canvas to fit the screen
-function resize(){
-    //  Match the size of the container for the canvas
-    var rect = canvas.getBoundingClientRect();
+function resize() {
+  //  Match the size of the container for the canvas
+  var rect = canvas.getBoundingClientRect();
 
-    ctx.canvas.width = rect.width;
-    ctx.canvas.height = rect.height;
+  ctx.canvas.width = rect.width;
+  ctx.canvas.height = rect.height;
 }
 
 // draw on canvas
-function draw(e){
-    // Check for mouse click
-    if (e.buttons !== 1) return
-    
-    ctx.beginPath();
+function draw(e) {
+  // Check for mouse click
+  if (e.buttons !== 1) return;
 
-    ctx.lineWidth = 2;
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = "#000000";
+  ctx.beginPath();
 
-    ctx.moveTo(pos.x, pos.y);
-    setPosition(e);
-    ctx.lineTo(pos.x, pos.y);
+  ctx.lineWidth = 2;
+  ctx.lineCap = "round";
+  ctx.strokeStyle = "#000000";
 
-    ctx.stroke();
+  ctx.moveTo(pos.x, pos.y);
+  setPosition(e);
+  ctx.lineTo(pos.x, pos.y);
+
+  ctx.stroke();
 }
 
-document.getElementById("save").addEventListener('click', function(){
-    // get canvas info
-    var canvasContents = canvas.toDataURL(); // Image data URL
-    var data = { image: canvasContents, date: Date.now()};
-    var string = JSON.stringify(data);
+document.getElementById("send").addEventListener("click", function () {
+  // get canvas info
+  var canvasContents = canvas.toDataURL(); // Image data URL
+  var data = { image: canvasContents, date: Date.now() };
+  var string = JSON.stringify(data);
 
-    // Create a blod from the json
-    var file = (new Blob([string], {
-        type: 'application/json'
-    }));
+  // Create a blod from the json
 
-    // Create an object URL for the blob
-    var fileURL = URL.createObjectURL(file);
+  // Create an object URL for the blob
+  //   var fileURL = URL.createObjectURL(file);
 
-    // Create a temporary anchor element to create a download
-    var a = document.createElement('a');
-    a.href = fileURL;
-    a.download = 'canvas-drawing.json';
-    document.body.appendChild(a);
-    a.click(); // Click the download button programatically
+  //   Create a temporary anchor element to create a download
+  //   var a = document.createElement("a");
+  //   a.href = fileURL;
+  //   a.download = "canvas-drawing.json";
 
-    document.body.removeChild(a);
-    URL.revokeObjectURL(fileURL);
+  //   document.body.appendChild(a);
+  //   a.click(); // Click the download button programatically
+  socket.emit("chat message", string);
+  //   document.body.removeChild(a);
+  //   URL.revokeObjectURL(fileURL);
 });
 
-document.getElementById("load").addEventListener('change', function(){
-    if(this.files[0]){
-        // read the contents of the first file in the input field
-        reader.readAsText(this.files[0]);
-    }
-})
+document.getElementById("load").addEventListener("change", function () {
+  if (this.files[0]) {
+    // read the contents of the first file in the input field
+    reader.readAsText(this.files[0]);
+  }
+});
 
-reader.onload = function () {
-    var data = JSON.parse(reader.result); //turns JSON into a usable object
-    var image = new Image();
-    image.onload = function () {
-        ctx.clearRect(0,0, canvas.width, canvas.height);
-        ctx.drawImage(image,0,0);
-    }
-    image.src = data.image;
-}
+// reader.onload = function () {
+//   var data = JSON.parse(reader.result); //turns JSON into a usable object
+//   var image = new Image();
+//   image.onload = function () {
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     ctx.drawImage(image, 0, 0);
+//   };
+//   image.src = data.image;
+// };
+socket.on("chat message", (msg) => {
+  console.log("Chat message");
+  const messages = document.getElementById("messages");
+  const img = document.createElement("img");
+  messages.appendChild(img);
+  var file = new Blob([msg], {
+    type: "application/json",
+  });
+  console.log(file);
+  var data = JSON.parse(msg);
+  var image = new Image();
+
+  img.src = data.image;
+});
--- server.js
+++ server.js
@@ -2,8 +2,7 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const path = require("path");
-const http = require('http');
-
+const http = require("http");
 
 require("dotenv").config();
 
@@ -11,7 +10,7 @@ const app = express();
 const PORT = process.env.port || 3000;
 
 const server = http.createServer(app);
-const {Server} = require("socket.io");
+const { Server } = require("socket.io");
 const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -20,11 +19,14 @@ app.use(expressLayouts);
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 // console.log(process.env) // remove this after you've confirmed it is working
+// console.log(io);
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
 
 app.use("/", require("./routes"));
 
-io.on('connection', (socket) => {
-    console.log("a user connected");
-});
-
-app.listen(PORT, () => console.log(`server is running on: ${PORT}`));
+server.listen(PORT, () => console.log(`server is running on: ${PORT}`));
